9be6e12c6e1f584fa4ef7e832327c1ddda6b11a3
wingfaili <liwingfai92@gmail.com>
2017-03-31 19:06:37 +0800

    float btn

--- app/src/main/java/com/cuhk/ieproject/MainActivity.java
+++ app/src/main/java/com/cuhk/ieproject/MainActivity.java
@@ -13,6 +13,8 @@ import android.net.SSLSessionCache;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Vibrator;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Display;
@@ -154,7 +156,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 //        test();
         setContentView(R.layout.activity_main);
-
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Go to setting page?", Snackbar.LENGTH_LONG)
+                        .setAction("Confirm", new View.OnClickListener(){
+                            @Override
+                            public void onClick(View view) {
+                                startActivity(new Intent(MainActivity.this, SettingActivity.class));
+                            }
+                        }).show();
+            }
+        });
         setupReferences();
         setupGesture();
         setupSize();
